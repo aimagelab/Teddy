c674c17bc35f41fe3d8ec4db97166bef9e3d2f24
Vittorio Pippi <219424@studenti.unimore.it>
2024-02-21 09:30:29 +0100

    Update style_text in eval_page generation

--- train.py
+++ train.py
@@ -281,7 +281,8 @@ def train(rank, args):
                 # same_author_imgs = make_grid(batch['same_author_imgs'], nrow=1, normalize=True, value_range=(-1, 1))
                 # other_author_imgs = make_grid(batch['other_author_imgs'], nrow=1, normalize=True, value_range=(-1, 1))
 
-                eval_page = teddy.generate_eval_page(batch['gen_text'], batch['style_text'], batch['style_img'])
+                style_text = [' ' for _ in batch['style_text']] if args.no_style_text else batch['style_text']
+                eval_page = teddy.generate_eval_page(batch['gen_text'], style_text, batch['style_img'])
 
             collector['time/epoch_inference'] = time.time() - epoch_start_time
             collector['ocr_loss_real'] = ocr_loss_real
