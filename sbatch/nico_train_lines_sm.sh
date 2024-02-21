38e14102af2663e65a85da38198b71ec70b5382a
Vittorio Pippi <219424@studenti.unimore.it>
2024-02-21 09:13:37 +0100

    Update function to handle edge cases

---
+++ sbatch/nico_train_lines_sm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --partition=all_usr_prod
+#SBATCH -e /work/FoMo_AIISDH/vpippi/Teddy/jobs/teddy_%j.err
+#SBATCH -o /work/FoMo_AIISDH/vpippi/Teddy/jobs/teddy_%j.out
+#SBATCH --mem=64G
+#SBATCH --cpus-per-task=16
+#SBATCH -J teddy
+#SBATCH --account=fomo_aiisdh
+#SBATCH --time=12:00:00
+#SBATCH --constraint="gpu_A40_48G"
+
+cd /work/FoMo_AIISDH/vpippi/Teddy || exit
+# scontrol update JobID="$SLURM_JOB_ID" name="teddy"
+srun /homes/$(whoami)/.conda/envs/teddy/bin/python train.py --batch_size 8 --root_path /work/FoMo_AIISDH/vpippi/Teddy/files/datasets/ --datasets iam_lines_sm --wandb --tag teddy_lines_sm --eval_dataset iam_eval  
