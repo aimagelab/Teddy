583522000423a95adde3a6a4a07a4525e3e70b53
Vittorio Pippi <219424@studenti.unimore.it>
2024-02-28 18:30:18 +0100

    Update job name in train_ocr1.sh script

--- sbatch/train_ocr1.sh
+++ sbatch/train_ocr1.sh
@@ -4,7 +4,7 @@
 #SBATCH -e /work/FoMo_AIISDH/vpippi/Teddy/jobs/teddy_%j.err
 #SBATCH -o /work/FoMo_AIISDH/vpippi/Teddy/jobs/teddy_%j.out
 #SBATCH --mem=32G
-#SBATCH -J ocr0
+#SBATCH -J ocr1
 #SBATCH --account=fomo_aiisdh
 #SBATCH --time=1-00:00:00
 #SBATCH --constraint="gpu_RTX6000_24G|gpu_RTXA5000_24G"
@@ -12,4 +12,4 @@
 
 cd /work/FoMo_AIISDH/vpippi/Teddy || exit
 # scontrol update JobID="$SLURM_JOB_ID" name="teddy"
-srun /homes/$(whoami)/.conda/envs/teddy/bin/python train.py --root_path /work/FoMo_AIISDH/vpippi/Teddy/files/datasets/ --wandb --tag eccv_1_ocr_freezed --resume --run_id ocr0 --ocr_scheduler alt1
+srun /homes/$(whoami)/.conda/envs/teddy/bin/python train.py --root_path /work/FoMo_AIISDH/vpippi/Teddy/files/datasets/ --wandb --tag eccv_1_ocr_freezed --resume --run_id ocr1 --ocr_scheduler alt1
